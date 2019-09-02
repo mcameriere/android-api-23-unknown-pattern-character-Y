ca8c6f74b26959c8df41bf6d0b1d0f2d805da4d0
mcameriere <31536331+mcameriere@users.noreply.github.com>
2019-09-02 15:14:33 +0200

    Update ExampleUnitTest.java

--- ExampleUnitTest.java
+++ ExampleUnitTest.java
@@ -13,9 +13,23 @@ public class ExampleUnitTest {
     public void lastDayOf2018_isWeek1_of2019() {
         SimpleDateFormat formatter = new SimpleDateFormat("Y"); // 'Y' = Week Year
         Calendar calendar = Calendar.getInstance();
-        calendar.set(2018,11,31);
+        calendar.set(2018, 11, 31);
         Date last_day_of_2018 = calendar.getTime();
+
         String formattedDate = formatter.format(last_day_of_2018);
+
         assertEquals("2019", formattedDate);
+    }
+
+    @Test
+    public void lastDayOf2018_isIn2018() {
+        SimpleDateFormat formatter = new SimpleDateFormat("y"); // 'y' = Year
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2018, 11, 31);
+        Date last_day_of_2018 = calendar.getTime();
+
+        String formattedDate = formatter.format(last_day_of_2018);
+
+        assertEquals("2018", formattedDate);
     }
 }
